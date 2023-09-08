425f6d294dfbf6e1027fc61c965f00105fb54c81
Chulwoong Choi <sentilemon02@gmail.com>
2023-09-08 12:51:37 +0900

    Add python_01_variable

---
+++ lecture/python_01_variable.py
@@ -0,0 +1,10 @@
+# 주석(Comment) : 단순 메모, 개발 실행 X
+
+# 출력 함수(print)
+#  - ()안의 값을 출력
+#  - 변수 값 확인 용도 또는 메세지 출력 용도
+print("Hello Python")
+
+# 문자열 타입(String Type)
+#  - Python: '', "" → String
+#  - C, JAVA: ''(Char), ""(String)
