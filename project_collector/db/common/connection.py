4584a1cc328386e8d5ca32a1567645b0de8b2666
ChoLong02 <sentilemon02@gmail.com>
2023-11-08 15:37:45 +0900

    Add db

---
+++ project_collector/db/common/connection.py
@@ -0,0 +1,29 @@
+# 데이터베이스(DB): 데이터를 효율적으로 저장하는 시스템(이론)
+# FileSystem: 폴더를 사용한 저장
+
+# 데이터베이스관리시스템(DBMS): DB 이론을 실제로 구현!
+
+# ** DBMS 종류
+#  1) 관계형 데이터베이스(RDB) : 전통적인(스키마:명세서)
+#   - ORACLE
+#   - MySQL
+#   - MariaDB
+
+# 2) NoSQL : NEW
+#   - MongoDB
+#   - Redis
+
+# ** DB 프로세스
+#  - DO: ID와 PW 저장
+#  Pycharm(Python)  -----------------  DB(MongoDB)
+#   1) Pycharm과 DB Connection 맺기
+#      - IP: 컴퓨터를 찾아갈 수 있는 주소!
+#      - PORT: 컴퓨터 내에서 특정 프로그램의 위치
+#      - ID and PW
+#   2) SQL을 사용해서 DB에 CRUD 작업 진행
+#      - SQL(구조질의어): DB와 소통할 수 있는 언어(반드시)
+#      Create(삽입)   → INSERT
+#      Read(조회)     → SELECT
+#      Update(수정)   → UPDATE
+#      Delete(삭제)   → DELETE
+
