d7fcfd5ea0aea95cb11b7baccb15e09ebc759bb1
ChoLong02 <sentilemon02@gmail.com>
2023-11-17 12:37:59 +0900

    Add review_date

--- project_daum_movie/main.py
+++ project_daum_movie/main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+from datetime import datetime, timedelta
 import time
 import re
 import math
@@ -32,7 +33,7 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
                           options=options)
 
 # URL 접속
-url = "https://movie.daum.net/moviedb/grade?movieId=165591"
+url = "https://movie.daum.net/moviedb/grade?movieId=169137"
 driver.get(url)
 time.sleep(2)
 
@@ -43,7 +44,8 @@ doc = BeautifulSoup(doc_html, "html.parser")
 
 # 영화 제목 수집
 movie_title = doc.select("span.txt_tit")[0].get_text()
-print(movie_title)
+print(f"영화제목: {movie_title}")
+
 
 
 
@@ -67,13 +69,31 @@ doc_html = driver.page_source
 doc = BeautifulSoup(doc_html, "html.parser")
 review_list = doc.select("ul.list_comment > li")
 
-print(len(review_list))
+print(f"전체리뷰: {len(review_list)}")
 
 for item in review_list:
     print("=" * 100)
-    review_score = item.select("")
+    review_score = item.select("div.ratings")[0].get_text()
     print(f"  - 평점: {review_score}")
-    # review_content = item.select("")
-    # print(f"  - 리뷰: {review_content}")
-    # review_writer = item.select("")
-    # print(f"  - 작성자: {review_writer}")
+    review_content = item.select("p.desc_txt")[0].get_text().strip()
+    # \n : 한 줄 개행
+    # 수집한 리뷰 개행 → 문자열 안에 \n 포함
+    review_content = re.sub("\n", "", review_content)
+    print(f"  - 리뷰: {review_content}")
+    review_writer = item.select("a.link_nick > span")[1].get_text()  # [댓글 작성자, 작성자, 댓글 모아보기]
+    print(f"  - 작성자: {review_writer}")
+
+    # 24시간 이내에 작성된 글은 날짜 → 예: 21시간전, 17시간전
+    # 실제 날짜 표기법 → 2023. 11. 17. 12:15
+    # 표기법: 21시간전 → 2023. 11. 17. 12:15
+    review_date = item.select("span.txt_date")[0].get_text()
+    #  = review_date → 17시간전 or 2023. 11. 17. 12:17
+    if len(review_date) < 7:
+        # 예) 17시간전 → 숫자만 추출: 17
+        reg_hour = int(re.sub(r"[^~0-9]", "", review_date))
+        # 예) 현재시간(2023.11.17 12:29) - 18 = 2023-11-16 18:29:23.232500
+        review_date = datetime.now() - timedelta(hours=reg_hour)
+        # 예) 2023-11-16 18:29:23.232500 → 2023. 11. 16. 18:29
+        review_date = review_date.strftime("%Y. %m. %d. %H:%M")
+    print(f"  - 날짜: {review_date}")
+
