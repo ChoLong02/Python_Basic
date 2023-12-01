84965f7548053a340ad652697517ed6223dd6633
ChoLong02 <sentilemon02@gmail.com>
2023-12-01 12:24:32 +0900

    Update sql_last_review

--- project_daum_movie/db/movie_dao.py
+++ project_daum_movie/db/movie_dao.py
@@ -1,4 +1,4 @@
-from db.common.connection import connection
+from .common.connection import connection
 
 
 # 리뷰 데이터 저장
@@ -20,4 +20,30 @@ def add_review(data):
         print(e)
     finally:
         # 5) 자원 반납
+        conn.close()
+
+
+# DB에 저장 된 리뷰 중 가장 최신의 날짜 가져오기
+def get_last_review():
+    conn = connection()
+    try:
+        curs = conn.cursor()
+        sql = """
+                SELECT *
+                FROM (
+                    SELECT DATE_FORMAT(STR_TO_DATE(reg_date, '%Y. %m. %d. %H:%i'), '%Y%m%d%H%i') AS int_regdate FROM tbl_review
+                    ORDER BY reg_date
+                ) EX1
+                ORDER BY int_regdate DESC LIMIT 1;
+              """
+        curs.execute(sql)
+        # INSERT, DELETE, UPDATE -> 결과 확인 X
+        # SELECT → DB로부터 데이터 받기
+        #  - 받는 데이터 단건   → fetchone()
+        #  - 받는 데이터 복수건 → fetchall()
+        last_date = curs.fetchone()
+        return last_date
+    except Exception as e:
+        print(e)
+    finally:
         conn.close()
